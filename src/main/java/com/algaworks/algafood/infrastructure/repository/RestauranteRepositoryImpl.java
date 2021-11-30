86a1b8dee4bfc9c2ad672fe36c1a1f9611cb7263
rodolfoHOk <rodolfo.okino@gmail.com>
2021-11-30 20:58:12 -0300

    Lombok e repositório de restaurantes

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@Override
+	public List<Restaurante> listar() {
+		return manager.createQuery("from Restaurante", Restaurante.class).getResultList();
+	}
+
+	@Override
+	public Restaurante buscar(Long id) {
+		return manager.find(Restaurante.class, id);
+	}
+
+	@Override
+	public Restaurante salvar(Restaurante restaurante) {
+		return manager.merge(restaurante);
+	}
+
+	@Override
+	public void remover(Restaurante restaurante) {
+		restaurante = buscar(restaurante.getId());
+		manager.remove(restaurante);
+	}
+
+}
