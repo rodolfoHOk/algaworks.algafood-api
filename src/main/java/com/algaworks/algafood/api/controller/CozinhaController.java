ed5bd92eed67a38259d5554e169db79f4bc8cd57
rodolfoHOk <rodolfo.okino@gmail.com>
2021-12-01 11:07:39 -0300

    Implementando content negotiation para retornar JSON ou XML - em metodos diferentes

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -15,14 +15,21 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 //@ResponseBody
 
 @RestController
-@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/cozinhas") //, produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar() {
+		System.out.println("Listando em json");
+		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public List<Cozinha> listarXML() {
+		System.out.println("Listando em xml");
 		return cozinhaRepository.listar();
 	}
 }
