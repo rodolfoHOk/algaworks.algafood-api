88bde11340a241bab6521eee00f90ecc30ae2ad4
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-08-16 18:52:44 -0300

    Descrevendo modelos de representações e suas propriedades

--- src/main/java/com/algaworks/algafood/api/v1/model/CidadeModel.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/CidadeModel.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.v1.model;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +12,12 @@ import lombok.Setter;
 @Setter
 public class CidadeModel extends RepresentationModel<CidadeModel>{
 	
+	@Schema(example = "1")
 	private Long id;
+	
+	@Schema(example = "Santos")
 	private String nome;
+	
 	private EstadoModel estado;
 	
 }
--- src/main/java/com/algaworks/algafood/api/v1/model/input/CidadeInput.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/input/CidadeInput.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import lombok.Setter;
 public class CidadeInput {
 
 	@NotBlank
+	@Schema(example = "Santos")
 	private String nome;
 	
 	@Valid
