de1ac4df0126c564d429431c2e8d26e33525b59f
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-08-16 19:29:51 -0300

    Descrevendo códigos de status de respostas em endpoints específicos

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -7,7 +7,10 @@ import com.algaworks.algafood.api.v1.model.input.CidadeInput;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -18,7 +21,10 @@ public interface CidadeControllerOpenApi {
 	@Operation(summary = "Lista as cidades")
 	CollectionModel<CidadeModel> listar();
 	
-	@Operation(summary = "Busca uma cidade por ID")
+	@Operation(summary = "Busca uma cidade por ID", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID da cidade inválido", content = @Content(schema = @Schema))
+	})
 	CidadeModel buscar(
 			@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
 	
