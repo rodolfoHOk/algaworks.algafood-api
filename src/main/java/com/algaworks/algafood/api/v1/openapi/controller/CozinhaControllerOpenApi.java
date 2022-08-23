eb62f501c827b0844bbe1de6caf3709209af39b0
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-08-23 18:57:53 -0300

    Corrigindo documentação com substituição de Pageable

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
@@ -5,13 +5,16 @@ import org.springframework.hateoas.PagedModel;
 
 import com.algaworks.algafood.api.v1.model.CozinhaModel;
 import com.algaworks.algafood.api.v1.model.input.CozinhaInput;
+import com.algaworks.algafood.core.springdoc.PageableParameter;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @SecurityRequirement(name = "security_auth")
 public interface CozinhaControllerOpenApi {
 
-	PagedModel<CozinhaModel> listar(Pageable pageable);
+	@PageableParameter
+	PagedModel<CozinhaModel> listar(@Parameter(hidden = true) Pageable pageable);
 
 	CozinhaModel buscar(Long cozinhaId);
 
---
+++ src/main/java/com/algaworks/algafood/core/springdoc/PageableParameter.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.core.springdoc;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Parameter(in = ParameterIn.QUERY, name="page", description = "Número da página",
+	schema = @Schema(type = "integer", defaultValue = "0"))
+@Parameter(in = ParameterIn.QUERY, name="size", description = "Quantidade de elementos por página",
+	schema = @Schema(type = "integer", defaultValue = "10"))
+@Parameter(in = ParameterIn.QUERY, name="sort", description = "Critério de ordenação: propriedade(asc|desc)",
+	schema = @Schema(type = "string"),
+	examples = {
+		@ExampleObject("nome"),
+		@ExampleObject("nome,asc"),
+		@ExampleObject("nome,desc"),
+})
+public @interface PageableParameter {
+
+}
