27e403b1bc5bdb150583126d6a6ba283e8fa349e
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-08-24 13:42:07 -0300

    Descrevendo parâmetros de projeções em endpoints de consultas

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteControllerOpenApi.java
@@ -10,13 +10,20 @@ import com.algaworks.algafood.api.v1.model.RestauranteBasicoModel;
 import com.algaworks.algafood.api.v1.model.RestauranteModel;
 import com.algaworks.algafood.api.v1.model.input.RestauranteInput;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @SecurityRequirement(name = "security_auth")
 public interface RestauranteControllerOpenApi {
 
+	@Operation(parameters = {
+		@Parameter(name = "projecao", description = "Nome da projeção", example = "apenas-nome", in = ParameterIn.QUERY)
+	})
 	CollectionModel<RestauranteBasicoModel> listar();
 
+	@Operation(hidden = true)
 	CollectionModel<RestauranteApenasNomeModel> listarApenasNomes();
 
 	RestauranteModel buscar(Long restauranteId);
